aa338790e5ceb482fe432f8e3e38207bffb80043
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-02-15 23:48:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 style={{color: "white"}}>The Evolution of<br /> Airnab fronted</h1>
+      <h2 style={{color: "yellow"}}>@spikerm</h2>
     </div>
   );
 }
